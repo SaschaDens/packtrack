659c308b558c08a1bc2d26d4d989ea68746ab885
Sascha  Dens <sascha.dens@telenet.be>
2014-02-13 14:10:07 +0100

    Quick email fix

--- app/Packtrack/Mailers/UserMailer.php
+++ app/Packtrack/Mailers/UserMailer.php
@@ -7,7 +7,7 @@ class UserMailer extends Mailer {
     public function welcome(User $user)
     {
         $view = 'emails.activation';
-        $data = [];
+        $data = array();
         $subject = 'Welcome to PackAndTrack';
 
         return $this->sendTo($user, $subject, $view, $data);
--- app/routes.php
+++ app/routes.php
@@ -5,6 +5,15 @@ Route::get('login', array('as' => 'login', 'uses' => 'SessionsController@create'
 Route::get('logout', 'SessionsController@destroy');
 Route::resource('sessions', 'SessionsController', array('only' => array('create', 'store', 'destroy')));
 
+Route::get('mail', function(){
+    $user = User::find(1);
+    $mail = new Packtrack\Mailers\UserMailer();
+
+    $mail->welcome($user);
+
+    return "Mail send";
+});
+
 Route::resource('register', 'UsersController');
 
 Route::resource('dashboard', 'DashboardController@index');
