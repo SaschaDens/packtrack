d3e0dcc92f941c207b520ffab75756d2f45d85d0
Development <development@HADES.(none)>
2014-02-28 10:28:08 +0100

    Update composer file

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,48 +3,50 @@
 @section('title', 'Pack And Track')
 
 @section('content')
-<div class="row">
-    <div class="col-md-3">
-        <div class="info-box slideLeft">
-            <span class="glyphicon glyphicon-phone fadeIn"></span>
-            <h3>Mobile App</h3>
-            <p>
-                Our mobile app makes it just as easy to track your package on the go then you can when you visit the
-                website at home.
-            </p>
+<div class="home">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="info-box slideLeft">
+                <span class="glyphicon glyphicon-phone fadeIn"></span>
+                <h3>Mobile App</h3>
+                <p>
+                    Our mobile app makes it just as easy to track your package on the go then you can when you visit the
+                    website at home.
+                </p>
+            </div>
         </div>
-    </div>
-    <div class="col-md-3">
-        <div class="info-box slideLeft">
-            <span class="glyphicon glyphicon-send fadeIn"></span>
-            <h3>Simple to use</h3>
-            <p>
-                The service we provide is easy to setup, use and manage. No hassle when sending a package or receiving one
-                through our services.
-            </p>
+        <div class="col-md-3">
+            <div class="info-box slideLeft">
+                <span class="glyphicon glyphicon-send fadeIn"></span>
+                <h3>Simple to use</h3>
+                <p>
+                    The service we provide is easy to setup, use and manage. No hassle when sending a package or receiving one
+                    through our services.
+                </p>
+            </div>
         </div>
-    </div>
-    <div class="col-md-3">
-        <div class="info-box slideLeft">
-            <span class="glyphicon glyphicon-time fadeIn"></span>
-            <h3>Fast Service</h3>
-            <p>
-                Our services rank among the best in delivery time and guaranteed delivery or return without cost with the
-                added benefit of near real time tracking of your package.
-            </p>
+        <div class="col-md-3">
+            <div class="info-box slideLeft">
+                <span class="glyphicon glyphicon-time fadeIn"></span>
+                <h3>Fast Service</h3>
+                <p>
+                    Our services rank among the best in delivery time and guaranteed delivery or return without cost with the
+                    added benefit of near real time tracking of your package.
+                </p>
+            </div>
         </div>
-    </div>
-    <div class="col-md-3">
-        <div class="info-box slideLeft">
-            <span class="glyphicon glyphicon-screenshot fadeIn"></span>
-            <h3>Tracking</h3>
-            <p>
-                Expecting a package, but you're not into waiting a whole day at home for it? Claim most of you day back with
-                the traceability of your package and be home when you need to!
-            </p>
+        <div class="col-md-3">
+            <div class="info-box slideLeft">
+                <span class="glyphicon glyphicon-screenshot fadeIn"></span>
+                <h3>Tracking</h3>
+                <p>
+                    Expecting a package, but you're not into waiting a whole day at home for it? Claim most of you day back with
+                    the traceability of your package and be home when you need to!
+                </p>
+            </div>
         </div>
-    </div>
 
+    </div>
 </div>
 {{--
 <div class="row">
